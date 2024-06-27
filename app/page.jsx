d29e6f063d9844ac272d8b5e55cffcd8d9c87409
James Le-Goff <jameslegoff92@gmail.com>
2024-06-27 08:51:42 -0400

    Renaned top nav file

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-import TopNav from "@/components/ui/TopNav";
+import TopNav from "@/components/ui/topNav";
 import BackgroundVideo from "@/components/ui/BackgroundVideo";
 import Nav from "@/components/ui/Nav";
 
