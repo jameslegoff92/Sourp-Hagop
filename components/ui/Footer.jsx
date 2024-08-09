1964cf8f06600c9a71fca32bd65deab81404b58d
Anayees Sarkes <1428398@crosemont.qc.ca>
2024-08-08 21:28:31 -0400

    Completed: Left side of footer

--- components/ui/Footer.jsx
+++ components/ui/Footer.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
-import styles from './Footer.module.css';
+import css from './Footer.module.css';
 
 const Footer = () => (
-    <footer className={styles.footer}>
-        <div className={styles.imageContainer}>
+    <footer className={css.footer}>
+        <div className={css.logoTextContainer}>
+            <img 
+                src="../images/logo.svg" 
+                alt="School Logo" 
+                className={css.logo}
+            />
+            <div>
+                <p className={`${css.logoText}`}> L’ÉCOLE ARMÉNIENNE </p>
+                <p className={`${css.logoText} ${css.logoTextLg}`}> SOURP HAGOP</p>
+                <p className={`${css.logoText} ${css.logoTextSm}`}>ÉCOLE PRIMAIRE V. ET A. SAFARIAN</p>
+                <p className={`${css.logoText} ${css.logoTextSmAlternate}`}>ÉCOLE SECONDAIRE PASDERMAJIAN</p>
+            </div>
+            <div className={css.bottomText}>
+                <p>L’École arménienne Sourp Hagop donne l’enseignement en français.</p>
+                <p>Elle conduit ses élèves du secondaire à l’obtention du Diplôme d’études secondaires du Québec.</p>
+            </div>
+            <img 
+                src="../images/feep_sceau_osbl.svg" 
+                alt="OSBL Logo" 
+                className={css.osblLogo}
+            />
+        </div>
+        <div className={css.imageContainer}>
             <img 
                 src="../images/school.svg" 
                 alt="Sourp Hagop School" 
-                className={styles.footerImage}
+                className={css.footerImage}
             />
         </div>
-        <div className={styles.blueOverlay}></div>
+        <div className={css.blueOverlay}></div>
     </footer>
 );
 
