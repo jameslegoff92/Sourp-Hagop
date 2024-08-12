c0c9d673cd4782be7768f5cc8fa24737ff667953
Anayees Sarkes <1428398@crosemont.qc.ca>
2024-08-12 16:35:13 -0400

    Created a seperated page for the faded background logo

--- app/history/page.jsx
+++ app/history/page.jsx
@@ -5,19 +5,7 @@ import Footer from "@/components/ui/Footer";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import { useRef, useEffect, useState } from "react";
 import Brightness1Icon from '@mui/icons-material/Brightness1';
-
-const BackgroundImageContainer = ({ children, style }) => (
-    <motion.div 
-        className="fixed left-0 right-0 flex justify-center items-center z-[-1]"
-        style={style}
-    >
-        {children}
-    </motion.div>
-);
-
-const BackgroundImage = ({ src }) => (
-    <img src={src} className="w-full max-w-[800px] h-auto object-contain opacity-10 ml-[200px]" />
-);
+import BackgroundLogo from "@/components/ui/BackgroundLogo";
 
 const ProgressBarContainer = ({ children, style }) => (
     <motion.div 
@@ -90,14 +78,12 @@ export default function History() {
 
     const ref = useRef(null);
     const [elementTop, setElementTop] = useState(0);
-    const [viewportHeight, setViewportHeight] = useState(0);
 
     useEffect(() => {
         const updateLayout = () => {
             if (ref.current) {
                 setElementTop(ref.current.offsetTop);
             }
-            setViewportHeight(window.innerHeight);
         };
 
         updateLayout();
@@ -113,18 +99,6 @@ export default function History() {
     );
 
     const scaledProgress = useTransform(scaleY, value => value * 1.0);
-
-    const backgroundY = useTransform(
-        scrollY,
-        [0, viewportHeight * 0.1],
-        ["150%", "15%"]
-    );
-
-    const backgroundOpacity = useTransform(
-        scrollY,
-        [0, viewportHeight * 0.2],
-        [0.3, 0.3]
-    );
 
     const circleCount = timelineData.length;
     const circleScales = [...Array(circleCount)].map((_, index) => {
@@ -154,9 +128,7 @@ export default function History() {
 
     return (
         <>
-            <BackgroundImageContainer style={{ y: backgroundY, opacity: backgroundOpacity }}>
-                <BackgroundImage src="../images/logo-big.svg" />
-            </BackgroundImageContainer>
+            <BackgroundLogo/>
             <Header imageSrc="../images/school.svg" headerText="NOTRE HISTOIRE" />
             <div className="h-20" ref={ref} />
             <ProgressBarContainer style={containerStyle}>
