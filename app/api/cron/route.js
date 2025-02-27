15d601120ca5d2cd5b9ef62b5e752f7785fa670e
James Le-Goff <jameslegoff92@gmail.com>
2025-02-26 21:15:18 -0500

    Refactor fetchData.js function

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -3,6 +3,13 @@ import connectToDatabase from "@/js/mongoose/connection.js";
 import Admin from "@/js/schemas/admin.js";
 import { getNewAccessToken } from "@/js/google/googleapi.js";
 
+
+
+/**
+ * API route to refresh Google's access token. This is a cron job that will run every 55 minutes.
+ *
+ * @returns {json} A json object indicating success or failure for the operation
+ */
 export async function GET() {
   try {
     await connectToDatabase();
--- libs/fetchData.js
+++ libs/fetchData.js
@@ -1,4 +1,18 @@
-export const fetchGoogleCalendarData = (primary, startDate, endDate, authToken) => {
+//dotenv is used to load the .env.local env variables from the nextjs environment into our nodejs environment
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
+import logger from "../js/logger/logger.js";
+
+/**
+ * Function to fetch event data from Google's Calendar API
+ *
+ * @param {string} primary This parameter determines which calendar you are pulling events from.
+ * @param {string} startDate Determines where you start collecting events. Needs to be in ISO8601 format.
+ * @param {string} endDate Determines where you end collecting events. Needs to be in ISO8601 format.
+ * @param {string} accessToken The OAuth Access Token you receive after authentication.
+ * @returns {array}  
+ */
+export const fetchGoogleCalendarData = async (primary, startDate, endDate, accessToken) => {
   const CALENDAR_ID = primary;
   const API_KEY = process.env.GOOGLE_API_KEY;
   const timeMin = startDate;
@@ -7,15 +21,17 @@ export const fetchGoogleCalendarData = (primary, startDate, endDate, authToken) 
 
   const headers = {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${authToken}`,
+    Authorization: `Bearer ${accessToken}`,
   };
 
-  return fetch(url, { headers })
-    .then((res) => res.json())
-    .then((data) => {
-      return data.items;
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+  try {
+    logger.info("Sending request to Google Calendar API.");
+    const res = await fetch(url, { headers });
+    logger.info("Response from Google Calendar API received.")
+    const data = await res.json();
+    logger.debug(data, "Parsed data from Google Calendar API.")
+    return data.items;
+  } catch (error) {
+    logger.error(error, "Error from Google Calendar API events request: ");
+  }
 };
