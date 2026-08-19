ae59b2c62dc783bb15c6f3ee6a17f1d6c4ede000
Anayees Sarkes <sarkes.anayees@gmail.com>
2026-08-19 00:48:43 -0400

    fix(i18n): correct filename case in git index

--- components/ui/Login.jsx
+++ components/ui/Login.jsx
@@ -1,5 +1,5 @@
 import { signIn } from "@/auth";
-import { Button } from "./button";
+import { Button } from "./Button";
 import { FcGoogle } from "react-icons/fc";
 import css from "./Login.module.css";
 
